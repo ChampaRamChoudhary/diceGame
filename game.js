3f52d9af704cad9882d5571a79e599b9f0b4e194
Champa Ram <110038047+ChampaRamChoudhary@users.noreply.github.com>
2022-08-01 22:47:07 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,28 @@
+
+
+function diceRoll(){
+    var randomNumber1= 6*Math.random();
+    var number1 = Math.ceil(randomNumber1);
+    var randomNumber2=6*Math.random();
+    var number2 = Math.ceil(randomNumber2);
+
+    var image1 = "dice"+number1+".png";
+    image1="./photos/"+ image1;
+    document.querySelectorAll("img")[0].setAttribute("src", image1);
+
+    var image2 = "dice"+number2+".png";
+    image2="./photos/"+image2;
+    document.querySelectorAll("img")[1].setAttribute("src", image2);
+
+
+    if (number1>number2){
+        document.querySelector("h3").innerHTML="Player '1' Win";
+    }
+    else if (number1<number2){
+        document.querySelector("h3").innerHTML="Player '2' Win";  
+    }
+    else{
+        document.querySelector("h3").innerHTML="Match Tie";
+    }
+}
+diceRoll();
